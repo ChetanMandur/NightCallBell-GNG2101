828359cefee0bc8baf3f877b020c71acc55fc7e0
Chetan <chetanmandur45@gmail.com>
2020-11-29 16:23:31 -0500

    Edited reciverCode, noticed case error by mental analysis. Feature still needs to be tested

--- recieverCode.py
+++ recieverCode.py
@@ -5,16 +5,19 @@ from gpiozero import RGBLED, Button
 
 led = RGBLED(red=9, green=10,blue=11)
 button = Button(2)
-button.when_pressed = button_pressed
+# button.when_pressed = button_pressed
 
 
-def button_pressed():
-    s.send("keyword receieved")
+# def button_pressed():
+#     s.send("keyword receieved")
 
 def data_received(data):
     print(data)
-    # s.send(data)
-    pause()
+    led.color=(1,0,0)
+    button.wait_for_press()
+    led.color=(0,0,0)
+    s.send(data)
+    # pause()
 
 
 s = BluetoothServer(data_received)
