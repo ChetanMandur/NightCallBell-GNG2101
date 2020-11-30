bd3c98f62a761f30a37091cd1d212f794bf87d94
Chetan <chetanmandur45@gmail.com>
2020-11-29 20:48:07 -0500

    Changed duration recording time to 5 seconds in bellCode.py

--- bellCode.py
+++ bellCode.py
@@ -22,7 +22,7 @@ while True:
         with sr.Microphone() as source:    
             # r.adjust_for_ambient_noise(source)
             r.energy_threshold = 0
-            data = r.record(source, duration=3)
+            data = r.record(source, duration=5)
             text = r.recognize_google(data,language='en')
             test = {"help", "swag"}
             if any(word in text for word in test):
