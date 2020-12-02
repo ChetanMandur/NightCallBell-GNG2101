e11d6fdfccbe02b5620221c1c541d8492d436d83
Chetan <chetanmandur45@gmail.com>
2020-12-02 15:19:14 -0500

    Editing button_pressed() on recieverCode.py so that waiting=False after button is pressed

--- recieverCode.py
+++ recieverCode.py
@@ -24,6 +24,7 @@ def button_pressed():
         print("accepting keyword")
         led.off()
         s.send("keyword receieved")
+        waiting = False
     else:
         print("bell did not send an active signal")
     
